c1be4582a8010768e3ba526925f70594ee7c8aec
又来了个玩家 <109119269+GuoYufan@users.noreply.github.com>
2025-01-20 18:09:10 +0800

    Add files via upload

--- 自动文件管理/自动生成源代码文件路径.py
+++ 自动文件管理/自动生成源代码文件路径.py
@@ -4,19 +4,19 @@ from 切换工作目录 import browse_directory
 
 def 自动生成源代码文件相对路径():
     browse_directory("❓main源文件所在目录在哪？")
-    main_path = os.getcwd()
+    project_path = os.getcwd()
 
     while True:
         browse_directory("❓下一批其他源文件所在目录在哪？",os.getcwd())
         src_path = os.getcwd()
-        # src_dir = re.search(re.escape(main_path)+os.sep+"(.+)",src_path).group(1)
-        # src_dir = src_path.replace(main_path+os.sep,"")
-        # src_dir = src_path.split(main_path+os.sep)[1]
-        src_dir = os.path.relpath(src_path,main_path)
+        # src_dir = re.search(re.escape(project_path)+os.sep+"(.+)",src_path).group(1)
+        # src_dir = src_path.replace(project_path+os.sep,"")
+        # src_dir = src_path.split(project_path+os.sep)[1]
+        src_dir = os.path.relpath(src_path,project_path)
         #input(src_dir)
 
         src_files = [f for f in os.listdir() if os.path.isfile(os.sep.join((src_path,f))) and f.endswith('.c')]
-        browse_directory("❓将生成的含有源代码文件相对路径的txt文件保存在哪？")
+        browse_directory("❓将生成的含有源代码文件相对路径的txt文件保存在哪？",project_path)
         if not input("❓确定开始生成或续写该txt文件?(Enter:否并退出)\n"):
             break
         with open('源代码文件相对路径.txt', 'a') as f:
